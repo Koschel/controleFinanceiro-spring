3a0493d722ea9f138a8f450090fe0c0ac08a9205
Diego Koschel <dasksoethe1@gmail.com>
2026-08-24 16:28:40 -0300

    Configuração da classe Movimentecao para o Swagger/OpenAPI

--- src/main/java/com/financeiro/controleFinanceiro/model/Movimentacao.java
+++ src/main/java/com/financeiro/controleFinanceiro/model/Movimentacao.java
@@ -1,5 +1,6 @@
 package com.financeiro.controleFinanceiro.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -9,29 +10,56 @@ import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 
 @Entity
+@Schema(description = "Representa uma movimentação financeira")
 public class Movimentacao {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(
+            description = "Identificador único da movimentação",
+            example = "1",
+            accessMode = Schema.AccessMode.READ_ONLY
+    )
     private Long id;
 
     @NotBlank(message = "A descrição é obrigatório.")
+    @Schema(
+            description = "Descrição da movimentação",
+            example = "Salário"
+    )
     private String descricao;
+
 
     @NotNull(message = "O valor é obrigatório")
     @Positive(message = "O valor precisa ser maior que Zero (0)")
+    @Schema(
+            description = "Valor da movimentação",
+            example = "5000.00"
+    )
     private BigDecimal valor;
 
     @NotNull(message = "O tipo é obrigatório")
     @Enumerated(EnumType.STRING)
+    @Schema(
+            description = "Tipo da movimentação",
+            example = "RECEITA"
+    )
     private TipoMovimentacao tipo;
 
     @NotNull(message = "Categoria é obrigatória" )
     @Enumerated(EnumType.STRING)
+    @Schema(
+            description = "Categoria da movimentação",
+            example = "SALARIO"
+    )
     private Categoria categoria;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "usuario_id")
+    @Schema(
+            description = "Usuário proprietário da movimentação",
+            accessMode = Schema.AccessMode.READ_ONLY
+    )
     private Usuario usuario;
 
     public Long getId() {
@@ -46,9 +74,12 @@ public class Movimentacao {
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
+
+    // getters e setters
     public BigDecimal getValor() {
         return valor;
     }
+    
     public void setValor(BigDecimal valor) {
         this.valor = valor;
     }
